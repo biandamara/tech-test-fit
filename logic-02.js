c33512de9747109bd4b3ac935a86b51308b5bc17
Bian Damara <bian.damara@gmail.com>
2022-12-16 12:56:26 +0700

    first commit

--- logic-02.js
+++ logic-02.js
@@ -10,3 +10,34 @@
 
 // c. Input: Masing-masing anak mendap(atkan uang jajan ya=ng be&rbeda.
 // Output: 4
+
+const countWords = (words) => {
+  // memisahkan kata dalam kalimat
+  const letr = words.split(" ");
+
+  // karakter spesial dengan manggunakan regex
+  const regex = /[`!@#$%^&*()_+\=\[\]{};':"\\|<>\/~]/;
+
+  // deklarasi untuk mencatat nilai
+  let counterWords = 0;
+
+  // looping untuk buat membaca tiap kata
+  letr.forEach((i) => {
+    // membaca tiap kata jika tidak terdapat special character
+    if (!regex.test(i)) {
+      // variabel pencatatan nilai apabila tiap kata sudah sesuai
+      counterWords++;
+    }
+  });
+
+  console.log(counterWords);
+};
+
+// soal a
+countWords("Saat meng*ecat tembok, Agung di_bantu oleh Raihan.");
+
+// soal b
+countWords("Berapa u(mur minimal[ untuk !mengurus ktp?");
+
+// soal c
+countWords("Masing-masing anak mendapat(kan uang jajan ya=ng be&rbeda.");
